52e6c722ea47ad957afc61ed8f7cdef86d2fb087
Michael Gardner <mgardne1@tctc.edu>
2017-03-01 21:01:51 -0500

    Changed the ID and NAMES of Input data
    I moved some things around and changed the names of the inputs so that I can pull that from the POST data.  If you work on the project sync the project first so that you get these updates! I will be using these NAMEs to pull the data from the form.

--- main.php
+++ main.php
@@ -8,29 +8,27 @@
 
 echo <<< HTML
 <html>
-
-<!-- <link rel="stylesheet" type="text/css" href="Bin101/css/style.css"/>  -->    <!-- Directory changes name --> 
-
-        <link rel="stylesheet" type="text/css" href="css/style.css"/>
-
 <head>
-	<header>
-		<!-- <img src="Bin101/img/logo.PNG" alt="Website Logo" align="top-left"> -->  <!-- Directory changes name -->
-            
+	<!-- All Links, Meta data, scripts, and css goes inside the <head> tags.  -->
+<!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+	
+<!-- <header> and <nav> shouldn't be in the head it should be in the <body>  -->
+
+</head>
+
+<body>
+	<header>           
             <img id="logo" src="img/logo.PNG" alt="Website Logo" align="top-left">
 	</header>
 
 	<nav>   
 	</nav>
-
-</head>
-
-<body>
-
+	
     <form action="main.php" method="post">
-		<h4 id="username"> Email <br> <input type="email" name="login email" required></h4>
-		<h4 id="password"> Password <br> <input type="password" name="login password" required></h4>	 <br>
-		<input type="submit" value="Log In" id="submit" >
+		<h4 id="username"> Email <br> <input type="email" name="lEmail" required></h4>
+		<h4 id="password"> Password <br> <input type="password" name="lPassword" required></h4>	 <br>
+		<input type="submit" value="logIn" id="submit" >
     </form>
     
     
@@ -38,24 +36,24 @@ echo <<< HTML
     
       <h2> Create a New Account </h2>
       
-        <input type="text" name="school name" placeholder="School Name" style="width:355px;" required>   <br>
+        <input type="text" name="sName" placeholder="School Name" style="width:355px;" required>   <br>
         
-        <input type="text" name="first name" placeholder="First name" required>
-        <input type="text" name="last name" placeholder="Last name" required>  <br>
+        <input type="text" name="fName" placeholder="First name" required>
+        <input type="text" name="lName" placeholder="Last name" required>  <br>
         
-        <input type="email" name="register email" placeholder="Email" style="width:355px;" required>   <br>
+        <input type="email" name="regEmail" placeholder="Email" style="width:355px;" required>   <br>
         
-        <input type="email" name="re-enter email" placeholder="Re-enter Email" style="width:355px;" required>    <br>
+        <input type="email" name="cfEmail" placeholder="Re-enter Email" style="width:355px;" required>    <br>
         
-        <input type="password" name="register password" placeholder="Password" style="width:355px;" required>   <br>
+        <input type="password" id="regPass" name="regPass" placeholder="Password" style="width:355px;" required>   <br>
         
-        <input type="password" name="re-enter password" placeholder="Confirm Password" style="width:355px;" required>   <br>
+        <input type="password" ="cfPassword" name="cfPassword" placeholder="Confirm Password" style="width:355px;" required>   <br>
         
         <h4 id="studentacc"> Student <input type="radio" name="account" value="1" required></h4>
         
         <h4 id="instructacc"> Instructor <input type="radio" name="account" value="2" required></h4>
         
-        <input type="submit" value="Create Account" id="create">
+        <input type="submit" value="createAccount" id="create">
     
     
     </form>
