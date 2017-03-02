245be5e3069e8ed5c7937e9e913619037493dad6
nmerck1 <nathanielmerck@yahoo.com>
2017-03-01 19:24:29 -0500

    Add files via upload

--- main.php
+++ main.php
@@ -9,55 +9,47 @@
 echo <<< HTML
 <html>
 
+<!-- <link rel="stylesheet" type="text/css" href="Bin101/css/style.css"/>  -->    <!-- Directory changes name --> 
+
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
 
 <head>
-<!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<header>
+		<!-- <img src="Bin101/img/logo.PNG" alt="Website Logo" align="top-left"> -->  <!-- Directory changes name -->
+            
+            <img id="logo" src="img/logo.PNG" alt="Website Logo" align="top-left">
+	</header>
+
+	<nav>   
+	</nav>
 
 </head>
 
 <body>
 
-    <header>
-		 <img src="img/logo.PNG" alt="Website Logo" align="top-left">
-	</header>
-
-	<nav>   <!--
-		<ul>
-			<li> <a href="index.php"> Home </a> </li>
-
-			<li> <a href="profile.php"> Profile </a> </li>
-
-			<li> <a href="register.php"> Register </a> </li>
-
-		</ul>   -->
-
-
-	</nav>
-
-    <form action="index.php" method="post">
+    <form action="main.php" method="post">
 		<h4 id="username"> Email <br> <input type="email" name="login email" required></h4>
 		<h4 id="password"> Password <br> <input type="password" name="login password" required></h4>	 <br>
 		<input type="submit" value="Log In" id="submit" >
     </form>
     
     
-    <form action="index.php" id="createAcc" name="createAcc" method="post">
+    <form action="main.php" id="createacc" method="post">
     
       <h2> Create a New Account </h2>
       
-        <input type="text" name="sName" placeholder="School Name" style="width:355px;" required>   <br>
+        <input type="text" name="school name" placeholder="School Name" style="width:355px;" required>   <br>
         
-        <input type="text" name="fName" placeholder="First name" required>
-        <input type="text" name="lName" placeholder="Last name" required>  <br>
+        <input type="text" name="first name" placeholder="First name" required>
+        <input type="text" name="last name" placeholder="Last name" required>  <br>
         
-        <input type="email" name="regEmail" placeholder="Email" style="width:355px;" required>   <br>
+        <input type="email" name="register email" placeholder="Email" style="width:355px;" required>   <br>
         
-        <input type="email" name="cfEmail" placeholder="Re-enter Email" style="width:355px;" required>    <br>
+        <input type="email" name="re-enter email" placeholder="Re-enter Email" style="width:355px;" required>    <br>
         
-        <input type="password" id="regPass" name="regPass" placeholder="Password" style="width:355px;" required>   <br>
+        <input type="password" name="register password" placeholder="Password" style="width:355px;" required>   <br>
         
-        <input type="password" ="cfPassword" name="cfPassword" placeholder="Confirm Password" style="width:355px;" required>   <br>
+        <input type="password" name="re-enter password" placeholder="Confirm Password" style="width:355px;" required>   <br>
         
         <h4 id="studentacc"> Student <input type="radio" name="account" value="1" required></h4>
         
