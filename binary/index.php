6fb05a9d848d89a0a455cebfe97934a1ebbcb6eb
coryw <lensflare.osx@gmail.com>
2017-03-02 15:00:02 -0500

    Created Chapter 3 Decimal-to-Binary quiz game. Created all changes within this PHP page, but I will create separate files for each module and script. For now this page will have all games on one page.

--- binary/index.php
+++ binary/index.php
@@ -6,7 +6,10 @@
 -->
 <html>
    <head>
+       <!-- Jquery and JQuery UI Libraries -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+       <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+       <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
    </head>
 
    <?php
@@ -29,7 +32,7 @@
       Enter an integer:<br>
       <input type="number" step="1" min="0" value="12" id="decimalInput" /><br>
       Binary Output:<br>
-      <input type="text" id="binaryOutput" value="1100" readonly="true">
+      <input type="text" id="binaryOutput" value="1100" readonly>
       
    </form> 
 
@@ -61,8 +64,60 @@
       <input type="button" id="2" class="binaryButton" value="0" />
       <input type="button" id="1" class="binaryButton" value="0" /><br>
       Decimal Output:<br>
-      <input type="text" id="decimalOutput" value="0" readonly="true">
+      <input type="text" id="decimalOutput" value="0" readonly>
       
-   </form> 
+   </form>
+
+   <?php
+   $chap3Questions = array("0"=>"0000", "1"=>"0001", "2"=>"0010",
+       "3"=>"0011", "4"=>"0100", "5"=>"0101", "6"=>"0110", "7"=>"0111",
+       "8"=>"1000", "9"=>"1001", "10"=>"1010", "11"=>"1011","12"=>"1100",
+       "13"=>"1101", "14"=>"1110", "15"=>"1111");
+   $questionNumbers = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+       13, 14, 15);
+   shuffle($questionNumbers);
+   ?>
+
+   <h1>Chapter 3 Quiz</h1>
+   <form>
+       <p id="questionNumber">Question 1</p>
+       <label for="question"><?php echo $questionNumbers[0]; ?></label><br>
+       <input type="text" id="question" class="quizInput" /><br>
+       <p id="success" hidden="true">You did it!</p>
+   </form>
+<p></p>
+
+   <script>
+       $(document).ready(function() {
+           var questionsArray = <?php echo json_encode($chap3Questions); ?>;
+           var questionNumbers = <?php echo json_encode($questionNumbers); ?>;
+           var currentQuestion = 0;
+           var quizzing = true;
+
+           $(".quizInput").keyup(function () {
+               if ($(this).val() == questionsArray[questionNumbers[currentQuestion]]) {
+                   $(this).css('background-color', 'LightGreen');
+                   currentQuestion++;
+                   if (currentQuestion >= 16) {
+                       $(this).attr("readonly", "true");
+                       $("#success").show();
+                       return;
+                   }
+                   $("label[for=question]").text(questionNumbers[currentQuestion]);
+                   $("#question").val("");
+
+                   $("#questionNumber").text("Question " + (currentQuestion + 1));
+
+               }
+               else if ($(this).val().length >= 4) {
+                   $(this).css('background-color', 'LightCoral');
+                   $(this).val("");
+               }
+               else
+                   $(this).css('background-color', 'white');
+           });
+       });
+
+   </script>
 
 </html>
