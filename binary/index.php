9a1e49b074c781a199cd6d3cee17cca17eb70875
coryw <lensflare.osx@gmail.com>
2017-03-02 12:07:20 -0500

    Switched from local JQuery file to Google's hosted version (testing PHPStorm's Git integration)

--- binary/index.php
+++ binary/index.php
@@ -6,7 +6,7 @@
 -->
 <html>
    <head>
-      <script src="jquery-3.1.1.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
    </head>
 
    <?php
