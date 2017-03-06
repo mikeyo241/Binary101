3ea18d6f73f842ab8a9a7aa0cf25de6d996042dd
mikeyo241 <mgardne1@tctc.edu>
2017-03-06 01:06:38 -0500

    This was with the previous commit
    asdfasdfasdfasdfasdf

--- functionlib.php
+++ functionlib.php
@@ -88,7 +88,37 @@ function checkUser()  {
     }
 
 }
+/** Function:       checkLogin
+ * Last Modified:   2 November 2016
+ * @param           $email - Email address  (40 characters MAX)
+ * @param           $pw -    Password    md5 Encrypted password      Always 32 characters long
+ * @return          bool - Will return true if the user exists and the password is correct.
+ * Description:     This function is used to determine if the Email and password entered on the
+ *                  login page match the username and password in the database.
+ */
 
+function checkLogin($id , $pass){
+//      ** Check input for database exploits **
+    $id = fixSql($id);
+    $pass = md5(fixSql($pass));
+//      *** Establish a connection to the database  ***
+    $link = dbConnect();
+
+//      *** Database Query's    ***
+    $qry = "SELECT * FROM ACCOUNT WHERE ACC_USERNAME = '$id'";
+
+    if($result = mysqli_query($link,$qry)) {                // Implement the query
+        if (mysqli_num_rows($result) == 1) {                // There can only be 1 entry for email no duplicates.
+            $res = mysqli_fetch_assoc($result);             // Put the result into an array
+            if($pass == $res['ACC_PASS'] && $id == $res['ACC_USERNAME']) return true;
+        }
+    }else {             // Query Failed - Error Messages Not shown !!!!
+        echo "Error: " . $qry . "<br>" . mysqli_error($link);
+        $link->close();
+        return false;
+    }
+    return false;
+}
 /*function addAccount($){
 
 
@@ -164,7 +194,7 @@ function createAccount ($fName, $mName, $lName, $maiden, $email, $pass, $accType
         $qry2 = "INSERT INTO EMAIL VALUES  ('$email','$userName')";
 
 //      *** Implement Query's   ***
-        mysqli_query($link, $qry);
+        mysqli_query($link,$qry);
         mysqli_query($link, $qry2);
 
 //      ***     Close Connection    ***
